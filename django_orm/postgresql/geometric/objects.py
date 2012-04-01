62567c7d81d105739924225696ad87ae9fc4759e
Andrei Antoukh <niwi@niwi.be>
2012-04-01 23:45:56 +0200

    Improved implementation of geometric objects.

--- django_orm/postgresql/geometric/objects.py
+++ django_orm/postgresql/geometric/objects.py
@@ -3,6 +3,7 @@
 import re
 from django.utils import simplejson as json
 from django.db import connection
+from psycopg2.extensions import adapt, register_adapter, AsIs, new_type, register_type
 
 rx_circle_float = re.compile(r'<\(([\d\.\-]*),([\d\.\-]*)\),([\d\.\-]*)>')
 rx_line = re.compile(r'\[\(([\d\.\-]*),\s*([\w\.\-]*)\),\s*\(([\d\.\-]*),\s*([\d\.\+]*)\)\]')
@@ -10,163 +11,6 @@ rx_point = re.compile(r'\(([\d\.\-]*),\s*([\d\.\-]*)\)')
 rx_box = re.compile(r'\(([\d\.\-]*),\s*([\d\.\-]*)\),\s*\(([\d\.\-]*),\s*([\d\.\-]*)\)')
 rx_path_identify = re.compile(r'^((?:\(|\[))(.*)(?:\)|\])$')
 
-
-class Point(tuple):
-    """ 
-    Class that rep resents of geometric point. 
-    """
-
-    def __init__(self, args):
-        if len(args) == 2:
-            super(Point, self).__init__(args)
-        else: 
-            raise ValueError("Max is 2 elements")
-        self._validate()
-
-    def _validate(self):
-        if not isinstance(self.x, (int, long, float)) \
-            or not isinstance(self.y, (int, long, float)):
-            raise ValueError("invalid data")
-
-    def __repr__(self):
-        return "<Point(%s,%s)>" % self
-
-    @property
-    def x(self):
-        return self[0]
-    
-    @property
-    def y(self):
-        return self[1]
-
-
-class Circle(tuple):
-    def __init__(self, args):
-        if len(args) == 3:
-            super(Circle, self).__init__(args)
-        else:
-            raise ValueError("invalid data")
-        self._validate()
-
-    def _validate(self):
-        if not isinstance(self.r, (int, long, float)):
-            raise ValueError("invalid data")
-
-    def __repr__(self):
-        return "<Circle(%s,%s)>" % (self.point, self.r)
-
-    @property
-    def r(self):
-        return self[2]
-
-    @property
-    def point(self):
-        return Point(self[:-1])
-
-    def to_box(self):
-        if hasattr(self, '_box'):
-            return self._box
-
-        cur = connection.cursor()
-        cur.execute("select box(%s) as _;", [self])
-        res = cur.fetchone()
-        cur.close()
-
-        if not res:
-            raise ValueError("Unexpected error")
-
-        self._box = res[0]
-        return res[0]
-
-
-class Lseg(tuple):
-    def __init__(self, args):
-        if len(args) == 4:
-            super(Lseg, self).__init__(args)
-        else:
-            raise ValueError("invalid content")
-
-    def __iter__(self):
-        yield tuple(self.init_point)
-        yield tuple(self.end_point)
-
-    def __repr__(self):
-        return "<Lseg(%s, %s)>" % \
-            (self.init_point, self.end_point)
-
-    @property
-    def init_point(self):
-        return Point(self[:2])
-
-    @property
-    def end_point(self):
-        return Point(self[2:])
-
-
-class Box(tuple):
-    def __init__(self, args):
-        if len(args) == 4:
-            super(Box, self).__init__(args)
-        else:
-            raise ValueError("invalid content")
-
-    def __repr__(self):
-        return "<Box(%s,%s),(%s,%s)>" % self
-
-    @property
-    def init_point(self):
-        return Point(self[:2])
-
-    @property
-    def end_point(self):
-        return Point(self[2:])
-
-    @property
-    def center_point(self):
-        if hasattr(self, '_center_point'):
-            return self._center_point
-
-        cur = connection.cursor()
-        cur.execute("select @@ %s;", [self])
-        res = cur.fetchone()
-        cur.close()
-
-        if not res:
-            raise ValueError("Unexpected error")
-
-        self._center_point = res[0]
-        return res[0]
-
-
-class Path(tuple):
-    closed = False
-
-    def __init__(self, args):
-        points = []
-        for item in args:
-            if isinstance(item, (tuple, list, Point)):
-                points.append(tuple(item))
-            else:
-                points = []
-                raise ValueError("invalid content")
-        
-        self.closed = isinstance(args, tuple)
-
-        if len(points) == 0:
-            raise ValueError("invalid content")
-
-        super(Path, self).__init__(points)
-    
-    def __repr__(self):
-        return "<Path(%s) closed=%s>" % (len(self), self.closed)
-
-
-class Polygon(Path):
-    def __repr__(self):
-        return "<Polygon(%s) closed=%s>" % (len(self), self.closed)
-
-
-from psycopg2.extensions import adapt, register_adapter, AsIs, new_type, register_type
 
 """ PYTHON->SQL ADAPTATION """
 
@@ -210,13 +54,6 @@ def adapt_polygon(path):
         for x, y in path]
 
     return AsIs(container % (",".join(points)))
-
-register_adapter(Point, adapt_point)
-register_adapter(Circle, adapt_circle)
-register_adapter(Box, adapt_box)
-register_adapter(Path, adapt_path)
-register_adapter(Polygon, adapt_polygon)
-register_adapter(Lseg, adapt_lseg)
 
 """ SQL->PYTHON ADAPTATION """
 
@@ -302,34 +139,222 @@ def cast_polygon(value, cur):
     ) for x, y in rx_point.findall(points)], closed=is_closed)
 
 
-cur = connection.cursor()
-cur.execute("SELECT NULL::point, NULL::circle, NULL::line, NULL::box, "
-            "NULL::path, NULL::polygon, NULL::lseg")
+ADAPT_MAPPER = {
+    'Point': adapt_point,
+    'Circle': adapt_circle,
+    'Box': adapt_box,
+    'Path': adapt_path,
+    'Polygon': adapt_polygon,
+    'Lseg': adapt_lseg,
+}
 
-point_oid, circle_oid, \
-line_oid, box_oid, \
-path_oid, polygon_oid, lseg_oid = \
-    cur.description[0][1], \
-    cur.description[1][1], \
-    cur.description[2][1], \
-    cur.description[3][1], \
-    cur.description[4][1], \
-    cur.description[5][1], \
-    cur.description[6][1]
 
-cur.close()
-connection.close()
+CAST_MAPPER = {
+    'Point': cast_point,
+    'Circle': cast_circle,
+    'Box': cast_box,
+    'Path': cast_path,
+    'Polygon': cast_polygon,
+    'Lseg': cast_lseg,
+}
 
-POINT = new_type((point_oid,), "POINT", cast_point)
-CIRCLE = new_type((circle_oid,), "CIRCLE", cast_circle)
-BOX = new_type((box_oid,), "BOX", cast_box)
-PATH = new_type((path_oid,), "PATH", cast_path)
-POLYGON = new_type((polygon_oid,), "POLYGON", cast_polygon)
-LSEG = new_type((lseg_oid,), "LSEG", cast_lseg)
+class GeometricMeta(type):
+    """
+    Base meta class for all geometryc types.
+    """
 
-register_type(POINT)
-register_type(CIRCLE)
-register_type(BOX)
-register_type(PATH)
-register_type(POLYGON)
-register_type(LSEG)
+    def __init__(cls, name, bases, attrs):
+        super(GeometricMeta, cls).__init__(name, bases, attrs)
+        cls._registed = False
+
+    def __call__(cls, *args, **kwargs):
+        instance = super(GeometricMeta, cls).__call__(*args, **kwargs)
+        if cls._registed:
+            return instance
+
+        if cls.type_name() not in ADAPT_MAPPER:
+            cls._registed = True
+            return instance
+
+        adapt_function = ADAPT_MAPPER[cls.type_name()]
+        cast_function = ADAPT_MAPPER[cls.type_name()]
+
+        register_adapter(cls, adapt_function)
+        cursor = connection.cursor()
+        cursor.execute(cls.sql_for_oid())
+        oid = cur.description[0][1]
+        cursor.close()
+        connection.close()
+
+        PGTYPE = new_type((oid,), ntype.upper(), cast_function)
+        register_type(PGTYPE)
+        cls._registed = True
+
+        return instance
+
+    def type_name(cls):
+        return unicode(cls.__name__)
+
+    def sql_for_oid(cls):
+        ntype = cls.type_name().lower()
+        return "SELECT NULL::%s" % (ntype)
+
+class Point(tuple):
+    """ 
+    Class that rep resents of geometric point. 
+    """
+    __metaclass__ = GeometricMeta
+
+    def __init__(self, args):
+        if len(args) == 2:
+            super(Point, self).__init__(args)
+        else: 
+            raise ValueError("Max is 2 elements")
+        self._validate()
+
+    def _validate(self):
+        if not isinstance(self.x, (int, long, float)) \
+            or not isinstance(self.y, (int, long, float)):
+            raise ValueError("invalid data")
+
+    def __repr__(self):
+        return "<Point(%s,%s)>" % self
+
+    @property
+    def x(self):
+        return self[0]
+    
+    @property
+    def y(self):
+        return self[1]
+
+
+class Circle(tuple):
+    __metaclass__ = GeometricMeta
+    def __init__(self, args):
+        if len(args) == 3:
+            super(Circle, self).__init__(args)
+        else:
+            raise ValueError("invalid data")
+        self._validate()
+
+    def _validate(self):
+        if not isinstance(self.r, (int, long, float)):
+            raise ValueError("invalid data")
+
+    def __repr__(self):
+        return "<Circle(%s,%s)>" % (self.point, self.r)
+
+    @property
+    def r(self):
+        return self[2]
+
+    @property
+    def point(self):
+        return Point(self[:-1])
+
+    def to_box(self):
+        if hasattr(self, '_box'):
+            return self._box
+
+        cur = connection.cursor()
+        cur.execute("select box(%s) as _;", [self])
+        res = cur.fetchone()
+        cur.close()
+
+        if not res:
+            raise ValueError("Unexpected error")
+
+        self._box = res[0]
+        return res[0]
+
+
+class Lseg(tuple):
+    __metaclass__ = GeometricMeta
+    def __init__(self, args):
+        if len(args) == 4:
+            super(Lseg, self).__init__(args)
+        else:
+            raise ValueError("invalid content")
+
+    def __iter__(self):
+        yield tuple(self.init_point)
+        yield tuple(self.end_point)
+
+    def __repr__(self):
+        return "<Lseg(%s, %s)>" % \
+            (self.init_point, self.end_point)
+
+    @property
+    def init_point(self):
+        return Point(self[:2])
+
+    @property
+    def end_point(self):
+        return Point(self[2:])
+
+
+class Box(tuple):
+    __metaclass__ = GeometricMeta
+    def __init__(self, args):
+        if len(args) == 4:
+            super(Box, self).__init__(args)
+        else:
+            raise ValueError("invalid content")
+
+    def __repr__(self):
+        return "<Box(%s,%s),(%s,%s)>" % self
+
+    @property
+    def init_point(self):
+        return Point(self[:2])
+
+    @property
+    def end_point(self):
+        return Point(self[2:])
+
+    @property
+    def center_point(self):
+        if hasattr(self, '_center_point'):
+            return self._center_point
+
+        cur = connection.cursor()
+        cur.execute("select @@ %s;", [self])
+        res = cur.fetchone()
+        cur.close()
+
+        if not res:
+            raise ValueError("Unexpected error")
+
+        self._center_point = res[0]
+        return res[0]
+
+
+class Path(tuple):
+    __metaclass__ = GeometricMeta
+    closed = False
+
+    def __init__(self, args):
+        points = []
+        for item in args:
+            if isinstance(item, (tuple, list, Point)):
+                points.append(tuple(item))
+            else:
+                points = []
+                raise ValueError("invalid content")
+        
+        self.closed = isinstance(args, tuple)
+
+        if len(points) == 0:
+            raise ValueError("invalid content")
+
+        super(Path, self).__init__(points)
+    
+    def __repr__(self):
+        return "<Path(%s) closed=%s>" % (len(self), self.closed)
+
+
+class Polygon(Path):
+    __metaclass__ = GeometricMeta
+    def __repr__(self):
+        return "<Polygon(%s) closed=%s>" % (len(self), self.closed)
