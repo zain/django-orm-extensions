d2ffdb291bde9f0b04e28cfbbf74963f710c2b28
Andrei Antoukh <niwi@niwi.be>
2012-04-04 21:14:21 +0200

    Remove unused code.

--- django_orm/cache/invalidator.py
+++ django_orm/cache/invalidator.py
@@ -11,15 +11,3 @@ def invalidate_object(instance, **kwargs):
         return
 
     cache.delete(instance.cache_key)
-    if hasattr(cache, 'keys'):
-        log.info("Searching querysets with this model...")
-        find_pattern = "*qs:default:table:%s*" % \
-            instance.__class__._meta.db_table
-        keys = cache.keys(find_pattern)
-        log.info("Found %s keys for querysets. Invalidating...", len(keys))
-        log.info("Keys: %s", str(keys))
-        cache.delete_many(keys)
-    else:
-        log.warning("Cache backend not suport keys method. "
-            "Without this method is imposible invalidate querysets. "
-            "This feature is disabled!")
