7978e9e1c8eeeb02825003f8de166473ae5cb02c
Andrei Antoukh <niwi@niwi.be>
2012-04-06 12:17:13 +0200

    Remove backends, now uses standard backends.

--- django_orm/backends/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.dispatch import receiver
-from django.db.backends.signals import connection_created
-
-@receiver(connection_created)
-def my_callback(sender, connection, signal, **kwargs):
-    #print "Connection created:", id(connection.connection)
-    pass
