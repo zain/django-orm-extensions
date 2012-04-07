2fa2b40655f856e06e115141083efa64065d2941
Andrei Antoukh <niwi@niwi.be>
2012-04-07 18:36:26 +0200

    Bug fixes.

--- django_orm/utils/statements.py
+++ django_orm/utils/statements.py
@@ -2,6 +2,7 @@
 
 from django.utils import tree
 from django.db.models.sql.datastructures import MultiJoin
+from django.db.models.fields import FieldDoesNotExist
 
 class CommonBaseTree(tree.Node):
     """
@@ -70,7 +71,7 @@ class OperatorTree(CommonBaseTree):
             return
         
         field, source, opts, join_list, last, _ = queryset.query.setup_joins(
-            parts, self.model._meta, query.get_initial_alias(), False)
+            parts, queryset.model._meta, query.get_initial_alias(), False)
         
         # Process the join chain to see if it can be trimmed
         col, _, join_list = queryset.query.trim_joins(source, join_list, last, False)
@@ -80,8 +81,26 @@ class OperatorTree(CommonBaseTree):
         # in order for zeros to be returned for those aggregates.
         for column_alias in join_list:
             queryset.query.promote_alias(column_alias, unconditional=True)
-        
-        node.field = (parts[-2], parts[-1])
+
+        # this works for one level of depth
+        #lookup_model = self.query.model._meta.get_field(parts[-2]).rel.to
+        #lookup_field = lookup_model._meta.get_field(parts[-1])
+
+        lookup_model = queryset.model
+        for counter, field_name in enumerate(parts):
+            try:
+                lookup_field = lookup_model._meta.get_field(field_name)
+            except FieldDoesNotExist:
+                parts.pop()
+                break
+            
+            try:
+                lookup_model = lookup_field.rel.to
+            except AttributeError:
+                parts.pop()
+                break
+
+        node.field = (lookup_model._meta.db_table, lookup_field.attname)
 
     def as_sql(self, qn, queryset):
         items, params = [], []
@@ -157,114 +176,111 @@ class Statement(object):
             raise ValueError("Invalid field value")
 
         params.update(self.extra)
-
-        print self.sql_template
-        print params
         return self.sql_template % params, self.args
 
 
-class BaseTree(CommonBaseTree):
-    def sql_condition(self, parts, value, qn):
-        raise NotImplemented("Can be reimplemented on subclass.")
-
-    def as_sql(self, qn, connection, query):
-        self.query = query
-        self.items = []
-        self.params = ()
-
-        for qobj in self.children:
-            if hasattr(qobj, 'as_sql'):
-                _sql, _params = qobj.as_sql(qn, connection, query)
-
-            else:
-                parts, value = qobj[0].split("__"), qobj[1]
-                _sql, _params = self.sql_condition(parts, value, qn)
-
-            self.items.append(_sql)
-            self.params += tuple(_params)
-        
-        if not self.items:
-            return None, ()
-
-        if len(self.items) == 1:
-            if self.negated:    
-                return self.negated_block(self.items[0]), self.params
-            return self.items[0],  self.params
-        
-        join_attr = " %s " % (self.connector)
-        result_sql = join_attr.join(self.items)
-
-        if self.negated:
-            return self.negated_block(result_sql), self.params
-
-        return result_sql, self.params
-
-    def key_to_quoted_field_name(self, parts, qn):
-        num_parts = len(parts)
-        if num_parts == 1:
-            return u"%s.%s" % (
-                qn(self.query.model._meta.db_table),
-                qn("__".join(parts))
-            )
-
-        if num_parts % 2 != 0:
-            raise ValueError("Incorrect number of lookups")
-        
-        # this works for one level of depth
-        #lookup_model = self.query.model._meta.get_field(parts[-2]).rel.to
-        #lookup_field = lookup_model._meta.get_field(parts[-1])
-
-        lookup_model = self.query.model
-        for counter, field_name in enumerate(parts):
-            try:
-                lookup_field = lookup_model._meta.get_field(field_name)
-            except FieldDoesNotExist:
-                parts.pop()
-                break
-            
-            try:
-                lookup_model = lookup_field.rel.to
-            except AttributeError:
-                parts.pop()
-                break
-
-        return u"%s.%s" % (
-            qn(lookup_model._meta.db_table), 
-            qn(lookup_field.attname),
-        )
-
-    def negated_block(self, sql):
-        return u"NOT (%s)" % (sql)
-
-    def _check_subnodes(self, node, query):
-        for child in node.children:
-            if isinstance(child, BaseTree):
-                self._check_subnodes(child, query)
-                continue
-
-            key, value = child
-            if "__" not in key:
-                continue
-
-            parts = key.split("__")
-
-            try:
-                field, target, opts, join_list, last, extra_filters = query.setup_joins(
-                    parts, query.model._meta, query.get_initial_alias(), True, True, allow_explicit_fk=True,
-                    can_reuse=None, negate=False,
-                    process_extras=True)
-            except MultiJoin as e:
-                pass
-
-    def add_to_query(self, query, alias=None):
-        self._check_subnodes(self, query)
-        query.where.add(ExtraWhereStatement(query, self), self.connector)
-
-    
-class ExtraWhereStatement(object):
-    def __init__(self, query, node):
-        self.node = node
-        self.query = query
-
-    def as_sql(self, qn, connection):
-        return self.node.as_sql(qn, connection, self.query)
+#class BaseTree(CommonBaseTree):
+#    def sql_condition(self, parts, value, qn):
+#        raise NotImplemented("Can be reimplemented on subclass.")
+#
+#    def as_sql(self, qn, connection, query):
+#        self.query = query
+#        self.items = []
+#        self.params = ()
+#
+#        for qobj in self.children:
+#            if hasattr(qobj, 'as_sql'):
+#                _sql, _params = qobj.as_sql(qn, connection, query)
+#
+#            else:
+#                parts, value = qobj[0].split("__"), qobj[1]
+#                _sql, _params = self.sql_condition(parts, value, qn)
+#
+#            self.items.append(_sql)
+#            self.params += tuple(_params)
+#        
+#        if not self.items:
+#            return None, ()
+#
+#        if len(self.items) == 1:
+#            if self.negated:    
+#                return self.negated_block(self.items[0]), self.params
+#            return self.items[0],  self.params
+#        
+#        join_attr = " %s " % (self.connector)
+#        result_sql = join_attr.join(self.items)
+#
+#        if self.negated:
+#            return self.negated_block(result_sql), self.params
+#
+#        return result_sql, self.params
+#
+#    def key_to_quoted_field_name(self, parts, qn):
+#        num_parts = len(parts)
+#        if num_parts == 1:
+#            return u"%s.%s" % (
+#                qn(self.query.model._meta.db_table),
+#                qn("__".join(parts))
+#            )
+#
+#        if num_parts % 2 != 0:
+#            raise ValueError("Incorrect number of lookups")
+#        
+#        # this works for one level of depth
+#        #lookup_model = self.query.model._meta.get_field(parts[-2]).rel.to
+#        #lookup_field = lookup_model._meta.get_field(parts[-1])
+#
+#        lookup_model = self.query.model
+#        for counter, field_name in enumerate(parts):
+#            try:
+#                lookup_field = lookup_model._meta.get_field(field_name)
+#            except FieldDoesNotExist:
+#                parts.pop()
+#                break
+#            
+#            try:
+#                lookup_model = lookup_field.rel.to
+#            except AttributeError:
+#                parts.pop()
+#                break
+#
+#        return u"%s.%s" % (
+#            qn(lookup_model._meta.db_table), 
+#            qn(lookup_field.attname),
+#        )
+#
+#    def negated_block(self, sql):
+#        return u"NOT (%s)" % (sql)
+#
+#    def _check_subnodes(self, node, query):
+#        for child in node.children:
+#            if isinstance(child, BaseTree):
+#                self._check_subnodes(child, query)
+#                continue
+#
+#            key, value = child
+#            if "__" not in key:
+#                continue
+#
+#            parts = key.split("__")
+#
+#            try:
+#                field, target, opts, join_list, last, extra_filters = query.setup_joins(
+#                    parts, query.model._meta, query.get_initial_alias(), True, True, allow_explicit_fk=True,
+#                    can_reuse=None, negate=False,
+#                    process_extras=True)
+#            except MultiJoin as e:
+#                pass
+#
+#    def add_to_query(self, query, alias=None):
+#        self._check_subnodes(self, query)
+#        query.where.add(ExtraWhereStatement(query, self), self.connector)
+#
+#    
+#class ExtraWhereStatement(object):
+#    def __init__(self, query, node):
+#        self.node = node
+#        self.query = query
+#
+#    def as_sql(self, qn, connection):
+#        return self.node.as_sql(qn, connection, self.query)
